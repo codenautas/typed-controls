43bf8782048f81aa552797e0a34492347a5d72bc
Emilio <emilioplatzer@gmail.com>
2015-12-06 12:51:59 -0300

    Cambio algunos nombres de carpetas y empiezo el test-connector.js

---
+++ test/test-connector.js
@@ -0,0 +1,22 @@
+"use script";
+
+describe("connector",function(){
+    describe("plain text conection without empty strings",function(){
+        var inputElement;
+        var almacen;
+        beforeEach(function(done){
+            inputElement = html.input().create();
+            almacen={ field1: 'one', field2: 'two', the_field: 'the value' };
+            done();
+        });
+        it("connects the input element with the almacen and populate",function(done){
+            connect({
+                element:inputElement,
+                almacen:almacen,
+                field:'the_field',
+                type:'text_no_empty',
+            });
+            expect(inputElement.value).to.be('the value');
+        });
+    });
+});
