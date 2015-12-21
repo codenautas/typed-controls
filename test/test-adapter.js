94f79b01054d405fc94ebef94c303db03ed8574a
Diego F <diegoefe@gmail.com>
2015-12-21 18:29:15 -0300

    Pequeña prueba de events

--- test/test-adapter.js
+++ test/test-adapter.js
@@ -222,26 +222,31 @@ describe("adapter",function(){
             });
         });
     });
-    describe.skip("events", function(){
-        function addEventToElem(elem) {
-            elem.addEventListener('customClick', function(e) {
+    if(! /PhantomJS/.test(window.navigator.userAgent)) {
+        describe("events", function(){
+            function sendClickTo(elem) {
+                    var e = new MouseEvent('click', { 'view': window, 'bubbles': true, 'cancelable': true });
+                    // var e = document.createEvent("MouseEvent");
+                    // e.initMouseEvent( "click",
+                                     // true /* bubble */, true /* cancelable */,
+                                     // window, null, 0, 0, 0, 0, /* coordinates */
+                                     // false, false, false, false, /* modifier keys */
+                                     // 0 /*left*/, null )
+                    elem.dispatchEvent(e);
+            }
+            it("must receive click and change the internal typed value", function(done){
+                var theElement = html.input({type:"checkbox"}).create();
+                Tedede.adaptElement(theElement,'boolean');
+                theElement.setTypedValue(null);
+                expect(theElement.getTypedValue()).to.be(null);
+                sendClickTo(theElement);
+                expect(theElement.getTypedValue()).to.be(true);
+                sendClickTo(theElement);
+                expect(theElement.getTypedValue()).to.be(false);
+                sendClickTo(theElement);
+                expect(theElement.getTypedValue()).to.be(true);
+                done();
             });
-        }
-        function sendClickTo(elem) {
-            elem.click();
-        }
-        it("must receive click and change the internal typed value", function(done){
-            var theElement = html.input({type:"checkbox"}).create();
-            Tedede.adaptElement(theElement,'boolean');
-            theElement.setTypedValue(false);
-            expect(theElement.getTypedValue()).to.be(null);
-            sendClickTo(theElement);
-            expect(theElement.getTypedValue()).to.be(true);
-            sendClickTo(theElement);
-            expect(theElement.getTypedValue()).to.be(false);
-            sendClickTo(theElement);
-            expect(theElement.getTypedValue()).to.be(true);
-            done();
         });
-    });
+    }
 });
