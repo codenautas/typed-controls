cca9d1b009628a440cbccb4ec698518650fe872e
Diego F.(i7) <diegoefe@gmail.com>
2015-12-22 11:26:21 -0300

    Los clicks simulados funcionan sólo con FireFox y Chrome

--- test/test-events.js
+++ test/test-events.js
@@ -1,31 +1,32 @@
 "use strict";
 
-if(! /Safari|PhantomJS/.test(window.navigator.userAgent)) {
-    describe("events", function(){
-        function sendClickTo(elem) {
-                // var e = new MouseEvent('click', { 'view': window, 'bubbles': true, 'cancelable': true });
-                var e = document.createEvent("MouseEvent");
-                e.initMouseEvent( "click",
-                                 true /* bubble */, true /* cancelable */,
-                                 window, null, 0, 0, 0, 0, /* coordinates */
-                                 false, false, false, false, /* modifier keys */
-                                 0 /*left*/, null )
-                // var e = document.createEvent('MouseEvents');
-                // e.initEvent( 'click', true, true );
-                elem.dispatchEvent(e);
+describe("events", function(){
+    function sendClickTo(elem) {
+        if(/Firefox|Chrome/.test(window.navigator.userAgent)) {
+            var e = document.createEvent("MouseEvent");
+            e.initMouseEvent( "click",
+                             true /* bubble */, true /* cancelable */,
+                             window, null, 0, 0, 0, 0, /* coordinates */
+                             false, false, false, false, /* modifier keys */
+                             0 /*left*/, null )
+            elem.dispatchEvent(e);
+        } else {
+            elem.click();
+            //var coords = {x:elem.offsetLeft, y:elem.offsetTop};
+            //page.sendEvent('click', coords.x + 1, coords.y + 1);
         }
-        it("must receive click and change the internal typed value", function(done){
-            var theElement = html.input({type:"checkbox"}).create();
-            Tedede.adaptElement(theElement,'boolean');
-            theElement.setTypedValue(null);
-            expect(theElement.getTypedValue()).to.be(null);
-            sendClickTo(theElement);
-            expect(theElement.getTypedValue()).to.be(true);
-            sendClickTo(theElement);
-            expect(theElement.getTypedValue()).to.be(false);
-            sendClickTo(theElement);
-            expect(theElement.getTypedValue()).to.be(true);
-            done();
-        });
+    }
+    it("must receive click and change the internal typed value", function(done){
+        var theElement = html.input({type:"checkbox"}).create();
+        Tedede.adaptElement(theElement,'boolean');
+        theElement.setTypedValue(null);
+        expect(theElement.getTypedValue()).to.be(null);
+        sendClickTo(theElement);
+        expect(theElement.getTypedValue()).to.be(true);
+        sendClickTo(theElement);
+        expect(theElement.getTypedValue()).to.be(false);
+        sendClickTo(theElement);
+        expect(theElement.getTypedValue()).to.be(true);
+        done();
     });
-}
+});
