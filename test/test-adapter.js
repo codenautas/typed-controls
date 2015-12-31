6378254e28d120d6887ce965da1b13d54577aa2d
Emilio <emilioplatzer@gmail.com>
2015-12-31 20:16:11 -0300

    Paso todas las pruebas de adaptar a la lista de datos

--- test/test-adapter.js
+++ test/test-adapter.js
@@ -11,6 +11,8 @@ var toTest = {
         invalidData:[
             {value:true},
             {value:new Date()},
+            // {value:'sarasa'},
+            // {value:'0'},
             {value:0},
             {value:32},
             {value:{}},
@@ -23,7 +25,7 @@ var toTest = {
             {value:null        , display:''          , },
             {value:42          , display:'42'        , },
             {value:0           , display:'0'         , },
-            {value:12345.125   , display:'hello'     , htmlDisplay: 
+            {value:12345.125   , display:'12345.125' , htmlDisplay: 
                 '<span class="number_miles">12</span>'+
                 '<span class="number_miles">345</span>'+
                 '<span class="number_dot">.</span>'+
@@ -33,7 +35,10 @@ var toTest = {
         invalidData:[
             {value:true},
             {value:new Date()},
+            {value:'sarasa'},
             {value:'0'},
+            // {value:0},
+            // {value:32},
             {value:{}},
             {value:[]},
             {value:/regexp/},
@@ -41,14 +46,44 @@ var toTest = {
     },
     "date": {
         validData:[
+            {value:null                   , display:'' },
+            {value:new Date(2015,12-1,31) , display:'31/12/2015'  , htmlDisplay:
+                '<span class="date_day">31</span>'+
+                '<span class="date_sep">/</span>'+
+                '<span class="date_month">12</span>'+
+                '<span class="date_sep">/</span>'+
+                '<span class="date_year">2015</span>',
+                valueISO: '2015-12-31'
+            },
         ],
         invalidData:[
+            {value:true},
+            // {value:new Date()},
+            {value:'sarasa'},
+            {value:'0'},
+            {value:0},
+            {value:32},
+            {value:{}},
+            {value:[]},
+            {value:/regexp/},
         ]
     },
     "boolean": {
         validData:[
+            {value:null , display:''  , htmlDisplay:''},
+            {value:true , display:'Sí', htmlDisplay:'<span class="bool_true">Sí</span>' },
+            {value:false, display:'no', htmlDisplay:'<span class="bool_false">no</span>'},
         ],
         invalidData:[
+            // {value:true},
+            {value:new Date()},
+            {value:'sarasa'},
+            {value:'0'},
+            {value:0},
+            {value:32},
+            {value:{}},
+            {value:[]},
+            {value:/regexp/},
         ]
     } 
 };
@@ -112,11 +147,26 @@ describe("adapter",function(){
     Object.keys(BestTypes).forEach(function(typeName){ BestTypes[typeName].domFixtures.forEach(function(def){
         describe("for type '"+typeName+"' and fixture "+JSON.stringify(def), function(){
             var theElement;
+            var theElementErr;
             var skip;
             beforeEach(function(done){
-                theElement = Tedede.createFromFixture(def).create();
-                Tedede.adaptElement(theElement,typeName);
-                document.body.appendChild(theElement);
+                try{
+                    theElement = Tedede.createFromFixture(def).create();
+                }catch(err){
+                    theElementErr = err;
+                    theElement = null;
+                }
+                skip = NOT_SUPPORTED_SITUATION({
+                    when: def.attributes.type==='date',
+                    must: theElement && theElement.type==='date',
+                    description: 'input of type date',
+                    excluding: 'Firefox 34.0, Firefox 43.0, IE 11.0'.split(', '),
+                    context: theElementErr
+                });
+                if(!skip){
+                    Tedede.adaptElement(theElement,typeName);
+                    document.body.appendChild(theElement);
+                }
                 done();
             });
             toTest[typeName].validData.map(function(data){
@@ -128,7 +178,14 @@ describe("adapter",function(){
                     }
                     if(!data.multiline){
                         var inspect = def.tagName==='input' || def.tagName==='textarea'?'value':'textContent';
-                        expect(theElement[inspect]).to.eql(coalesce(data.value,''));
+                        if(def.attributes.type==='checkbox'){
+                            expect(theElement.indeterminate).to.be(data.value===null);
+                            expect(theElement.checked).to.be(data.value===true);
+                        }else if(def.attributes.type==='date' && data.value!=null){
+                            expect(theElement.value).to.eql(data.valueISO);
+                        }else{
+                            expect(theElement[inspect]).to.eql('display' in data?data.display:data.value);
+                        }
                     }
                     expect(theElement.getTypedValue()).to.eql(data.value);
                     if(typeName==='text'){
@@ -149,128 +206,4 @@ describe("adapter",function(){
             });
         });
     }); });
-    describe("for boolean type",function(){
-        [
-            {tagName:'div'  , type:''        , html:true , show:true }, 
-            {tagName:'input', type:'text'    , html:false, show:true }, 
-            {tagName:'input', type:'checkbox', html:false, show:false}
-        ].forEach(function(def){
-            var theElement;
-            beforeEach(function(done){
-                theElement = html[def.tagName]({type:def.type}).create();
-                Tedede.adaptElement(theElement,'boolean');
-                done();
-            });
-            [
-                {value:true , display:'Sí', htmlDisplay:'<span class="bool_true">Sí</span>' },
-                {value:false, display:'no', htmlDisplay:'<span class="bool_false">no</span>'},
-                {value:null , display:''  , htmlDisplay:''},
-            ].map(function(data){
-                it("sets and get "+data.value+" in div",function(){
-                    theElement.setTypedValue(data.value);
-                    if(def.show){
-                        expect(coalesce(theElement.value, theElement.textContent)).to.be(data.display);
-                    }else{
-                        expect(theElement.indeterminate).to.be(data.value===null);
-                        expect(theElement.checked).to.be(data.value===true);
-                    }
-                    if(def.html){
-                        expect(theElement.innerHTML).to.be(data.htmlDisplay);
-                    }
-                    expect(theElement.getTypedValue()).to.be(data.value);
-                });
-            });
-            it("reject invalid value",function(){
-                var UNTOUCH = true;
-                theElement.setTypedValue(UNTOUCH);
-                expect(function(){
-                    theElement.setTypedValue('sarasa');
-                }).to.throwError(/Not a boolean in input/);
-                expect(theElement.getTypedValue()).to.be(UNTOUCH);
-            });
-        });
-    });
-    describe("for date type",function(){
-        [
-            {tagName:'div'  , type:''        , html:true , show:true }, 
-            {tagName:'input', type:'text'    , html:false, show:true }, 
-            {tagName:'input', type:'date'    , html:false, show:true }
-        ].forEach(function(def){
-            var theElement;
-            var skip;
-            beforeEach(function(done){
-                var theElementErr=null;
-                try{
-                    theElement = html[def.tagName]({type:def.type}).create();
-                }catch(err){
-                    theElement = null;
-                    theElementErr = err;
-                }
-                /*
-                var theBox = html.div({style:'border: 1px solid green;'},[
-                    html.span({style:'font-size:80%; color:#DDD;'},JSON.stringify(def))                    
-                ]).create();
-                theBox.appendChild(theElement);
-                document.body.appendChild(theBox); 
-                */
-                skip = NOT_SUPPORTED_SITUATION({
-                    when: def.type==='date',
-                    must: theElement && theElement.type==='date',
-                    description: 'input of type date',
-                    excluding: 'Firefox 34.0, Firefox 43.0, IE 11.0'.split(', '),
-                    context: theElementErr
-                });
-                if(!skip){
-                    Tedede.adaptElement(theElement,'date');
-                }
-                done();
-            });
-            [
-                {value:null                 , display:''  , htmlDisplay:''},
-                {value:new Date(2015,12-1,31) , display:'31/12/2015'  , htmlDisplay:
-                    '<span class="date_day">31</span>'+
-                    '<span class="date_sep">/</span>'+
-                    '<span class="date_month">12</span>'+
-                    '<span class="date_sep">/</span>'+
-                    '<span class="date_year">2015</span>',
-                    valueISO: '2015-12-31'
-                },
-            ].map(function(data){
-                it("sets and get "+data.value+" in div",function(){
-                    if(skip) return;
-                    theElement.setTypedValue(data.value);
-                    if(def.html){
-                        expect(theElement.innerHTML).to.be(data.htmlDisplay);
-                    }
-                    if(def.show){
-                        if(def.type==='date' && data.value!=null){
-                            expect(theElement.value).to.eql(data.valueISO);
-                        }else{
-                            expect(coalesce(theElement.value, theElement.textContent)).to.eql(data.display);
-                        }
-                    }
-                    expect(theElement.getTypedValue()).to.eql(data.value);
-                });
-            });
-           [
-                {value:true},
-                {value:'sarasa'},
-                {value:0},
-                {value:32},
-                {value:{}},
-                {value:[]},
-                {value:/regexp/},
-            ].forEach(function(def){
-                it("reject invalid value "+def.value,function(){
-                    if(skip) return;
-                    var UNTOUCH = new Date(2001,12-1,20);
-                    theElement.setTypedValue(UNTOUCH);
-                    expect(function(){
-                        theElement.setTypedValue(def.value);
-                    }).to.throwError(/Not a date in input/);
-                    expect(theElement.getTypedValue()).to.eql(UNTOUCH);
-                });
-            });
-        });
-    });
 });
