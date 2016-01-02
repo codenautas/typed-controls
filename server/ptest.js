2aa4109a897100a0e39c5d280ffb7f09e7f69e99
Emilio <emilioplatzer@gmail.com>
2016-01-02 18:18:34 -0300

    Agrego el punto y el menos para obtener null en un checkbox.

--- server/ptest.js
+++ server/ptest.js
@@ -1,5 +1,7 @@
 console.log('starting phantom');
 
+var expect = require('expect.js');
+
 var page = require('webpage').create();
 console.log('The default user agent is ' + page.settings.userAgent);
 page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.34 (KHTML, like Gecko) PhantomJS/1.9.8 Safari 5.1.7';
@@ -8,15 +10,23 @@ console.log('using ' + page.settings.userAgent);
 var pageName = 'http://localhost:43091/demo';
 
 function getInfo(id){
-    return page.evaluate(function(id){
+    return JSON.parse(page.evaluate(function(id){
         var bool1 = document.getElementById(id);
         var rect = bool1.getBoundingClientRect();
-        return {
+        // console.log('bool1',bool1.value, bool1.checked, bool1.indeterminate, bool1.getTypedValue(), bool1.type, bool1.tagName);
+        // var j = {
+        //     value: bool1.getTypedValue(),
+        //     cx: Math.floor((rect.left+rect.right)/2),
+        //     cy: Math.floor((rect.top+rect.bottom)/2),
+        // };
+        // console.log(j);
+        // console.log(JSON.stringify(j));
+        return JSON.stringify({
             value: bool1.getTypedValue(),
             cx: Math.floor((rect.left+rect.right)/2),
             cy: Math.floor((rect.top+rect.bottom)/2),
-        };
-    }, id);
+        });
+    }, id));
 }
 
 page.open(pageName, function(status, moreInfo) {
@@ -24,20 +34,59 @@ page.open(pageName, function(status, moreInfo) {
         console.log('Unable to access page', pageName, status, moreInfo);
         phantom.exit();
     }else{
-        var bool1 = getInfo('bool1'); 
-        console.log('first value',bool1.value);
-        page.sendEvent('click', bool1.cx, bool1.cy);
-        console.log('click', bool1.cx, bool1.cy);
-        //setTimeout(function(){
-            bool1 = getInfo('bool1'); 
-            console.log('then value',bool1.value);
-            page.render('server/local-capture.png', {format: 'png', quality: '100'});
-            phantom.exit();
-        //},1000);
+        runTests(page);
     }
 });
 
 
 page.onConsoleMessage = function(msg) {
-  console.log('#',msg);
+    console.log('#',msg);
+}
+
+function runTests(page){
+    function sendKey(key){
+        page.sendEvent('keydown', key);
+        page.sendEvent('keyup', key);
+    }
+    try{
+        var bool1 = getInfo('bool1'); 
+        expect(bool1.value).to.be(false);
+        page.sendEvent('click', bool1.cx, bool1.cy);
+        console.log('click', bool1.cx, bool1.cy);
+        bool1 = getInfo('bool1'); 
+        expect(bool1.value).to.be(true);
+        sendKey(page.event.key.Space);
+        // page.sendEvent('keypress', page.event.key.Space);
+        bool1 = getInfo('bool1'); 
+        expect(bool1.value).to.be(false);
+        sendKey(page.event.key.Delete);
+        bool1 = getInfo('bool1'); 
+        expect(bool1.value).to.be(null);
+        sendKey(page.event.key.Space);
+        bool1 = getInfo('bool1'); 
+        expect(bool1.value).to.be(false);
+        sendKey(page.event.key.Space);
+        bool1 = getInfo('bool1'); 
+        expect(bool1.value).to.be(true);
+        sendKey(page.event.key.Backspace);
+        bool1 = getInfo('bool1'); 
+        expect(bool1.value).to.be(null);
+        sendKey(page.event.key.Space);
+        bool1 = getInfo('bool1'); 
+        expect(bool1.value).to.be(false);
+        sendKey(page.event.key.Minus);
+        bool1 = getInfo('bool1'); 
+        expect(bool1.value).to.be(null);
+        sendKey(page.event.key.Space);
+        bool1 = getInfo('bool1'); 
+        expect(bool1.value).to.be(false);
+        sendKey(page.event.key.Period);
+        bool1 = getInfo('bool1'); 
+        expect(bool1.value).to.be(null);
+        page.render('server/local-capture.png', {format: 'png', quality: '100'});
+    }catch(err){
+        console.log('ERROR. TESTING');
+        console.log(err.stack);
+    }
+    phantom.exit();
 }
