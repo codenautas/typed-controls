6d1e0a5e7a980c240954bea81e4c4c71ae98baf2
Emilio <Platzer>
2016-01-06 10:35:02 -0300

    Saco factor común y arreglo los null que transformaba a "".

--- server/ctest.js
+++ server/ctest.js
@@ -12,18 +12,19 @@ casper.on("resource.error", function(msg, trace) {
     this.echo("Res.Error: " + msg);
 });
 
-function getInfo(elemName) {
-    return casper.page.evaluate(function(id) {
+function getInfo(elemId) {
+    return JSON.parse(casper.page.evaluate(function(id) {
         var theElement = document.getElementById(id);
         var rect = theElement.getBoundingClientRect();
-        return {
+        return JSON.stringify({
             value: 'getTypedValue' in theElement?theElement.getTypedValue():'not adapted',
             cx: Math.floor((rect.left+rect.right)/2),
             cy: Math.floor((rect.top+rect.bottom)/2),
             isActive: document.activeElement === theElement
-        }
-    }, elemName);
+        })
+    }, elemId));
 }
+
 function sendEvent(event, elem) {
     casper.page.sendEvent(event, elem.cx, elem.cy);
 }
@@ -48,41 +49,32 @@ function sendText(text) {
 var keys = null;
 var testUrl = 'http://localhost:43091/demo';
 
+
 casper.test.begin("Test checkbox", function(test) {
     casper.start(testUrl, function() {
+        var boolName = 'bool1';
+        function testKey(key, expected, description){
+            sendKey(key);
+            var bool1 = getInfo(boolName);
+            test.assertEquals(bool1.value, expected, description );
+        }
         keys = casper.page.event.key;
         test.assertTitle('tedede demo', 'titulo correcto');
         test.assertExists('#bool1', 'tengo bool1');
-        var boolN = 'bool1';
-        var bool1 = getInfo(boolN);
+        var bool1 = getInfo(boolName);
         test.assertEquals(bool1.value , false, "default value is false");
         sendEvent('click', bool1);
-        bool1 = getInfo(boolN);
+        bool1 = getInfo(boolName);
         test.assertEquals(bool1.value , true, "one click sets it to true");
-        sendKey(keys.Space);
-        bool1 = getInfo(boolN);
-        test.assertEquals(bool1.value , false, "space sets to false");
-        sendKey(keys.Delete);
-        bool1 = getInfo(boolN);
-        test.assert(! bool1.value, "delete sets null");
-        sendKey(keys.Space);
-        bool1 = getInfo(boolN);
-        test.assertEquals(bool1.value, false, "space sets to false (2)");
-        sendKey(keys.Backspace);
-        bool1 = getInfo(boolN);
-        test.assert(! bool1.value, "backspace sets null");
-        sendKey(keys.Space);
-        bool1 = getInfo(boolN);
-        test.assertEquals(bool1.value , false, "space sets to false (3)");
-        sendKey(keys.Minus);
-        bool1 = getInfo(boolN);
-        test.assert(! bool1.value, "minus sets null");
-        sendKey(keys.Space);
-        bool1 = getInfo(boolN);
-        test.assertEquals(bool1.value , false, "space sets to false (4)");
-        sendKey(keys.Period);
-        bool1 = getInfo(boolN);
-        test.assert(! bool1.value, "period sets null");
+        
+        testKey(keys.Space, false, "space sets to false");
+        testKey(keys.Delete, null, "delete sets null");
+        testKey(keys.Space, false, "space sets to false (2)");
+        testKey(keys.Backspace, null, "backspace sets null");
+        testKey(keys.Space, false, "space sets to false (3)");
+        testKey(keys.Minus, null, "minus sets null");
+        testKey(keys.Space, false, "space sets to false (4)");
+        testKey(keys.Period, null, "period sets null");
     }).run(function() {
         test.done();
     });    
