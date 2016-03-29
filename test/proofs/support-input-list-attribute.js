ab55ef174d80bcc657fd9b0d592d39aa5f09348f
Diego F.(i7) <diegoefe@gmail.com>
2016-03-29 10:40:28 -0300

    Prueba de concepto para <input list="milista" />

--- test/proofs/support-input-list-attribute.js
+++ test/proofs/support-input-list-attribute.js
@@ -1,9 +1,26 @@
 function supportInputListAttribute() {
     try {
         var elemento = document.createElement('input');
-        elemento.list='id-list'; // da error
-        document.write("elemento.list ["+elemento-list+"]")
+        elemento.type='text';
+        var datalist = document.createElement('datalist');
+        datalist.id = "lista";
+        //elemento.list='lista'; // da error
+        elemento.setAttribute('list','lista'); // ok 
+        var marcasAuto = ['Ford','Wolkswagen','Fiat'];
+        for(var marca in marcasAuto) {
+            var option = document.createElement('option');
+            option.label = marcasAuto[marca];
+            option.value = marcasAuto[marca];
+            datalist.appendChild(option);
+        }
+        if(false) {
+            document.body.appendChild(elemento);
+            document.body.appendChild(datalist);
+        }
         return true;
     }
-    catch(err) { return false; }
+    catch(err) {
+        document.write("error:"+err+"<br>\n")
+        return false;
+    }
 }
