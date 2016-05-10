3fc9aa268d20fe55fd406fe9f701ace3def836fd
Emilio <Platzer>
2016-05-10 16:46:37 -0300

    Para moreoptions

--- test/test-create-from-fixture.js
+++ test/test-create-from-fixture.js
@@ -27,3 +27,25 @@ describe("create-from-fixture",function(){
         element.create();
     });
 });
+
+describe("optionsCtrl",function(){
+    it.only("include more info", function(){
+        var ctrl = Tedede.optionsCtrl({
+            typeName: 'enum',
+            options:[
+                {option:1, label:'one'},
+                {option:2, label:'two', more:true}
+            ]
+        })
+        var expected = html.div({ 'tedede-option-group': 'simple-option' },[
+            html.div([html.input({"type":"radio","value":1}), html.label({"for-value":1}, "one"),html.br()]),
+            html.div([html.input({"type":"radio","value":2}), html.label({"for-value":2}, "two"),html.span({"class":"option-more-info"}),html.br()]),
+        ]);
+        console.log('---------------');
+        console.log(selfExplain.assert.allDifferences(ctrl, expected));
+        expect(ctrl).to.eql(expected);
+        var element = ctrl.create();
+        // Tedede.adaptElement
+        // expect element.moreinfo[2].className==='option-more-info'
+    });
+});
